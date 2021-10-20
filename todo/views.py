c934edfb55ea8a93e11d3524f7f977fea831f360
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-10-20 20:29:44 +0300

    created first model

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 def home(request):
     return render(request, "todo/home.html")
+
+
+
+def todo_list(request):
+    todos = Todo.objects.all()
+
+    context = {
+        'todos': todos
+    }
+
+    return render(request, "todo/list.html", context)
